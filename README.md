59051fba76591b4f6a53323981d1cdbe2b7f94cd
Lovro Ilijasic <lovro@toptal.com>
2018-10-15 14:17:33 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# CIEDE2000
-Python implementation of the CIEDE2000 color difference algorithm
+# Python CIEDE2000  
+**Python implementation of the CIEDE2000 color difference algorithm**
+
+A Python function for calculating how similar two colors are, based on the paper [The CIEDE2000 Color-Difference Formula: Implementation Notes, Supplementary Test Data, and Mathematical Observations](http://www.ece.rochester.edu/~gsharma/ciede2000/), by Gaurav Sharma, Wencheng Wu, and Edul N. Dalal.
+
+### Usage
+
+```
+from ciede2000 import CIEDE2000
+
+print(CIEDE2000((50, 2.6772, -79.7751), (50, 0.0000, -82.7485)))
+print(CIEDE2000((50, 0, 0), (50.0000, -1, 2)))
+print(CIEDE2000((50, 2.5, 0), (73, 25, -18)))
+```
+
+In order to use the function, you must first transform the image into L*a*b* color space (using OpenCV, for example):
+
+```
+import cv2
+
+image = cv2.imread("./some_image.jpeg")
+image = np.float32(image)
+image *= 1./255
+Lab = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
+L, a, b = cv2.split(Lab)
+```
---
+++ ciede2000.py
@@ -0,0 +1,71 @@
+import math
+
+def CIEDE2000(Lab_1, Lab_2):
+    '''Calculates CIEDE2000 color distance between two CIE L*a*b* colors'''
+    C_25_7 = 6103515625 # 25**7
+    
+    L1, a1, b1 = Lab_1[0], Lab_1[1], Lab_1[2]
+    L2, a2, b2 = Lab_2[0], Lab_2[1], Lab_2[2]
+    C1 = math.sqrt(a1**2 + b1**2)
+    C2 = math.sqrt(a2**2 + b2**2)
+    C_ave = (C1 + C2) / 2
+    G = 0.5 * (1 - math.sqrt(C_ave**7 / (C_ave**7 + C_25_7)))
+    
+    L1_, L2_ = L1, L2
+    a1_, a2_ = (1 + G) * a1, (1 + G) * a2
+    b1_, b2_ = b1, b2
+    
+    C1_ = math.sqrt(a1_**2 + b1_**2)
+    C2_ = math.sqrt(a2_**2 + b2_**2)
+    
+    if b1_ == 0 and a1_ == 0: h1_ = 0
+    elif a1_ >= 0: h1_ = math.atan2(b1_, a1_)
+    else: h1_ = math.atan2(b1_, a1_) + 2 * math.pi
+    
+    if b2_ == 0 and a2_ == 0: h2_ = 0
+    elif a2_ >= 0: h2_ = math.atan2(b2_, a2_)
+    else: h2_ = math.atan2(b2_, a2_) + 2 * math.pi
+
+    dL_ = L2_ - L1_
+    dC_ = C2_ - C1_    
+    dh_ = h2_ - h1_
+    if C1_ * C2_ == 0: dh_ = 0
+    elif dh_ > math.pi: dh_ -= 2 * math.pi
+    elif dh_ < -math.pi: dh_ += 2 * math.pi        
+    dH_ = 2 * math.sqrt(C1_ * C2_) * math.sin(dh_ / 2)
+    
+    L_ave = (L1_ + L2_) / 2
+    C_ave = (C1_ + C2_) / 2
+    
+    _dh = abs(h1_ - h2_)
+    _sh = h1_ + h2_
+    C1C2 = C1_ * C2_
+    
+    if _dh <= math.pi and C1C2 != 0: h_ave = (h1_ + h2_) / 2
+    elif _dh  > math.pi and _sh < 2 * math.pi and C1C2 != 0: h_ave = (h1_ + h2_) / 2 + math.pi
+    elif _dh  > math.pi and _sh >= 2 * math.pi and C1C2 != 0: h_ave = (h1_ + h2_) / 2 - math.pi 
+    else: h_ave = h1_ + h2_
+    
+    T = 1 - 0.17 * math.cos(h_ave - math.pi / 6) + 0.24 * math.cos(2 * h_ave) + 0.32 * math.cos(3 * h_ave + math.pi / 30) - 0.2 * math.cos(4 * h_ave - 63 * math.pi / 180)
+    
+    h_ave_deg = h_ave * 180 / math.pi
+    if h_ave_deg < 0: h_ave_deg += 360
+    elif h_ave_deg > 360: h_ave_deg -= 360
+    dTheta = 30 * math.exp(-(((h_ave_deg - 275) / 25)**2))
+    
+    R_C = 2 * math.sqrt(C_ave**7 / (C_ave**7 + C_25_7))  
+    S_C = 1 + 0.045 * C_ave
+    S_H = 1 + 0.015 * C_ave * T
+    
+    Lm50s = (L_ave - 50)**2
+    S_L = 1 + 0.015 * Lm50s / math.sqrt(20 + Lm50s)
+    R_T = -math.sin(dTheta * math.pi / 90) * R_C
+
+    k_L, k_C, k_H = 1, 1, 1
+    
+    f_L = dL_ / k_L / S_L
+    f_C = dC_ / k_C / S_C
+    f_H = dH_ / k_H / S_H
+    
+    dE_00 = math.sqrt(f_L**2 + f_C**2 + f_H**2 + R_T * f_C * f_H)
+    return dE_00
